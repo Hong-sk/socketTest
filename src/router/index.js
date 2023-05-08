ea93e1aae60e55f92123f3dfe2de35fccc801b1b
mike <sooyun9600@naver.com>
2023-05-08 10:56:01 +0900

    sockjs import 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SocketTestView from "@/components/SocketTestView.vue";
+import SocketTestTwo from "@/components/socketTestTwo.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: SocketTestView
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: SocketTestTwo
     },
     {
       path: '/about',
